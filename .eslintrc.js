9d57aeaba78422b0df5b5f2aa086489829ca1e63
Stefan Hagen <stefan@hagen.link>
2022-04-18 18:37:39 +0200

    Added opinions

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,21 +3,21 @@ module.exports = {
   env: {
     node: true,
     es2021: true,
-    "vue/setup-compiler-macros": true,
+    'vue/setup-compiler-macros': true,
   },
   extends: [
-    "eslint:recommended",
-    "@vue/prettier",
-    "@vue/typescript/recommended",
-    "plugin:vue/vue3-essential",
-    "plugin:storybook/recommended",
-    "plugin:markdown/recommended",
+    'eslint:recommended',
+    '@vue/prettier',
+    '@vue/typescript/recommended',
+    'plugin:vue/vue3-essential',
+    'plugin:storybook/recommended',
+    'plugin:markdown/recommended',
   ],
   parserOptions: {
     ecmaVersion: 2020,
   },
   rules: {
-    "no-console": "warn",
-    "@typescript-eslint/explicit-module-boundary-types": "off",
+    'no-console': 'warn',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
   },
-};
+}
